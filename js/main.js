9e50687d3be8b4b10881ebecc9e1fcda9d3a6d59
Hasib787 <mr.hasib45@gmail.com>
2021-02-10 00:20:25 +0600

    added a function to show the lyrics

--- js/main.js
+++ js/main.js
@@ -12,11 +12,10 @@ const searchSongs = () => {
 const displaySongs = songs => {
     const songContainer = document.getElementById("song-container");
     songs.forEach(song => {
-        console.log(song)
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
-        songDiv.innerHTML = 
-        `<div class="col-md-9">
+        songDiv.innerHTML =
+            `<div class="col-md-9">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
             <audio controls>
@@ -24,8 +23,14 @@ const displaySongs = songs => {
             </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="displayLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>`;
         songContainer.appendChild(songDiv);
     });
+}
+
+
+//display lyrics
+const displayLyrics = (artist, title) =>{
+    console.log(artist, title);
 }
