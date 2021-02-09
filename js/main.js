3577547fec276bfc4a379e3b1f5a1fd205272327
Hasib787 <mr.hasib45@gmail.com>
2021-02-09 19:33:05 +0600

    added some function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const searchSongs = () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+//Load data
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySongs(data.data))
+}
+
+
+//Display songs from api
+const displaySongs = songs =>{
+    songs.forEach(song => {
+        console.log(song.title)
+    });
 }
