11b7b9aa0fbf5e58275d26e8c45c557c12c48649
Hasib787 <mr.hasib45@gmail.com>
2021-02-10 01:57:39 +0600

    added async await

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const searchSongs = () => {
+const searchSongs = async () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
     //Load data
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data))
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySongs(data.data)
 }
+
+// const searchSongs = () => {
+//     const searchText = document.getElementById('search-field').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+//     //Load data
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displaySongs(data.data))
+// }
 
 
 //Display songs from api
@@ -32,14 +41,14 @@ const displaySongs = songs => {
 
 
 //display lyrics
-const getLyrics = (artist, title) => {
+const getLyrics = async (artist, title) => {
     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayLyrics(data.lyrics))
+    const res = await fetch(url);
+    const data = await res.json();
+    displayLyrics(data.lyrics);
 }
 
 const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById('song-lyrics');
-    lyricsDiv.innerText= lyrics;
+    lyricsDiv.innerText = lyrics;
 }
